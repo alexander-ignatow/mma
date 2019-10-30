3b05d181bf4199f0dde48648b72209f4440c8240
Alex Ignatov <alexander.ignatow@gmail.com>
2019-10-30 11:20:12 +0200

    few more input tests

--- src/tests/ProcessorTier.test.js
+++ src/tests/ProcessorTier.test.js
@@ -11,12 +11,44 @@ test('empty input test', () => {
   expect(expectedError.argument).toBe('A')
 })
 
+test('missing arg test', () => {
+  const input1 = { B: 'false', c: 'true', D: '0', E: '0', f: '0' }
+  const input2 = { a: 'true', B: 'false', D: '0', E: '0', f: '0' }
+
+  const expectedError1 = new ProcessorTier().process(input1)
+  const expectedError2 = new ProcessorTier().process(input2)
+
+  // don't check message - check only 'argument' field
+  expect(expectedError1.result).toBe('error')
+  expect(expectedError1.argument).toBe('A')
+
+  expect(expectedError2.result).toBe('error')
+  expect(expectedError2.argument).toBe('C')
+})
+
+test('invalid input test', () => {
+  const input1 = { a: 'NOT VALID', B: 'false', c: 'true', D: '0', E: '0', f: '0' }
+  const input2 = { a: 'false', b: 'false', c: 'false', d: 'NOT VALID', e: '-4', f: '10' }
+
+  const expectedError1 = new ProcessorTier().process(input1)
+  const expectedError2 = new ProcessorTier().process(input2)
+
+  // don't check message - check only 'argument' field
+  expect(expectedError1.result).toBe('error')
+  expect(expectedError1.argument).toBe('A')
+
+  expect(expectedError2.result).toBe('error')
+  expect(expectedError2.argument).toBe('D')
+})
+
 test('valid input test', () => {
-  const validInput1 = { a: 'true', B: 'false', c: 'true', d: '0', e: '0', f: '0' }
-  const validInput2 = { a: 'false', B: 'false', c: 'false', d: '34.3', e: '-4', f: '10' }
-  const validInput3 = { a: 'fAlSe', B: 'TRUE', c: 'true', d: '23e-4', e: '9882', f: '-28787' }
+  const validInput1 = { a: 'true', B: 'false', c: 'true', D: '0', E: '0', f: '0' }
+  const validInput2 = { a: 'false', b: 'false', c: 'false', d: '34.3', e: '-4', f: '10' }
+  const validInput3 = { A: 'fAlSe', B: 'TRUE', C: 'true', D: '23e-4', E: '9882', F: '-28787' }
+  const validInput4 = { a: 'true', B: 'false', c: 'true', D: '0', E: '0', f: '0', ignoreExtraArg: 'does not matter what' }
 
   expect(new ProcessorTier().process(validInput1)).toStrictEqual(generateErrorNoValidOutput())
   expect(new ProcessorTier().process(validInput2)).toStrictEqual(generateErrorNoValidOutput())
   expect(new ProcessorTier().process(validInput3)).toStrictEqual(generateErrorNoValidOutput())
+  expect(new ProcessorTier().process(validInput4)).toStrictEqual(generateErrorNoValidOutput())
 })
